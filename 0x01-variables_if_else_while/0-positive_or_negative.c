24696513d71d2079e0a5ec7ebc2be5caa1840399
Mekash <mekashae2@gmail.com>
2022-06-04 12:35:47 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,10 +3,10 @@
 #include <time.h>
 /* more headers goes there */
 /* betty style doc for function main goes there */
-int main(void) /**
-	 	* Description: main return either positive or
-		* negative or zero values 
-		*/
+/**
+ * Description: the main return either positive or negative or zero values
+ * /
+int main(void)	
 {
 	int n;
 
