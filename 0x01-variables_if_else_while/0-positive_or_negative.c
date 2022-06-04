c5414a349ee53052157a7572015d0811c263997b
Mekash <mekashae2@gmail.com>
2022-06-04 08:57:50 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,9 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+
 /*the main return random number including positive, negative, and zero */
+
 int main(void) 
 {
 	int n;
@@ -20,7 +22,7 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
-	else if (n == 0) 
+	else (n == 0) 
 	{	
 		printf("%d is zero\n", n);
 	}
