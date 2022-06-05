0a4400b8050305e5580fa5cce6cdca4335df7525
Mekash <mekashae2@gmail.com>
2022-06-05 15:53:58 -0700

    Last digit of random number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,6 +20,6 @@ int main(void)
 	if (lastD == 0)
 		printf("Last digit of %d is %d and is equal to 0\n", n, lastD);
 	else if (lastD < 6 && lastD != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastD);
+		printf("Last digit of %d is %d and is 0\n", n, lastD);
 	return (0);
 }
