c724b25fb184487c4fdef1e059e320b714cd121c
Mekash <mekashae2@gmail.com>
2022-06-05 16:16:04 -0700

    Lowercase Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main-Prints Lowercase Alphabet
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar(c);
+	return (0);
+}
