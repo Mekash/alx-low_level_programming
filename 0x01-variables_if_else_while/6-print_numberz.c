cbed3bf56523e49ccd343d13783bf07d83dedb91
Mekash <mekashae2@gmail.com>
2022-06-06 15:28:02 -0700

    print ditits from 0 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print the number from 0 to 9
+ *
+ * Return: Always(Success)
+ */
+int main(void)
+{
+	int 0;
+
+	for (n = '0'; n <= 9; n++)
+	{
+		putchar(0);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
