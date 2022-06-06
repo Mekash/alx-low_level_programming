bba907e6069a4819e36e1fd50d5f91750c2f8b51
Mekash <mekashae2@gmail.com>
2022-06-06 16:18:47 -0700

    print lowercase in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#nclude <stdio.h>
+
+/**
+ * main - print the lowercase in reverse order
+ *
+ * Return : Always (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c <= 'a'; c--)
+	{
+		utchar(c;
+	}
+
+	putchar(c;
+	return (0;
+}
