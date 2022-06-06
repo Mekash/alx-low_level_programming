9c6d60a499e34691aefd3f1f2c2aba50a2b32d0e
Mekash <mekashae2@gmail.com>
2022-06-06 06:39:58 -0700

    Lowercase except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - return alphabet in lowercase except q and e
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++);
+	{
+	   if (c != 'e' && c != 'q')
+	   {
+	   putchar (c);
+	   }
+	}
+	putchar('\n');
+
+	return (0);
+}
