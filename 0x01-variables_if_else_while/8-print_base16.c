b0a1ba935e581d306fee701c4df276db73635a18
Mekash <mekashae2@gmail.com>
2022-06-08 11:58:36 -0700

    print _putchar

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main -Print all the number of base 16 in lowercase
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(C);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ *main - Pprogram that prints _putchar
+ */
+int main(void)
+{
+	int char;
+
+		printf("_putchar");
+
+	return (0);
+}
