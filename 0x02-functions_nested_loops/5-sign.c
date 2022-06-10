701b419b4849cfd9795f0a03bfc5fdd4d7151771
Mekash <mekashae2@gmail.com>
2022-06-10 05:34:56 -0700

    _sign determine the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+  * _sign - prints the sign of a number
+  * @c: test the sign
+  * Return: 1 if n is greater than zero and 0 if n is zero and -1
+  * if n is less than zero
+  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('-');
+		return ('/');
+	}
+
+}
