080a2da605c354579b3ad43d5ef52e47d884c54c
Mekash <mekashae2@gmail.com>
2022-06-10 04:22:14 -0700

    -islower function return whether the alphabet is a - z

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-  * _islower return whether the alphabet is lowercase or not
-  *
+  * _islower test whether the alphabet is lowercase or not
+  * @c: character to test
   * Return: 1 if the alphabe is 1 or 0 otherwise
   */
 int _islower(int c)
