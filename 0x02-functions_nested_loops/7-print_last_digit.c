87ac5556c2ab29eebfc8bb828cfb85bacc96554f
Mekash <mekashae2@gmail.com>
2022-06-11 04:31:00 -0700

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,17 @@
 #include "main.h"
 /**
   * print_last_digit - print last digit of a mumber
-  * @n: last digit will be find
+  * @nld: last digit will be find
   * Return: The last digit
   */
 int print_last_digit(int nld)
 {
+	int nld;
+
 	if (nld < 0)
+	{
 		nld = nld * -1;
+	}
 	_putchar((nld % 10) + '0');
-	return (nld % 10); 
+	return (nld % 10);
 }
