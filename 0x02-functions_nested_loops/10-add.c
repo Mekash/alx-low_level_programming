817b9cf0f2c6179cb3a3fe26e8b86532d2c84374
Mekash <mekashae2@gmail.com>
2022-06-11 21:48:00 -0700

    add two integers and return result

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,10 +2,10 @@
 /**
  * add - the function add the two numbers
  * @one: the entity of the first number
- * @tow: the entity of the second number 
+ * @two: the entity of the second number
  * Return: one + two
  */
- int add(int one, int two)
+int add(int one, int two)
 {
 	return (one + two);
 }
