39f596a4fd97952eb365353d83098136fc4145dd
Mekash <mekashae2@gmail.com>
2022-06-15 09:04:27 -0700

    print the first 50 Fibonacci series

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,12 +6,12 @@
 int main(void)
 {
 	unsigned long count, i, j, k;
-	
+
 	i = 0;
 	j = 1;
 	for (count = 0; count < 50; count++)
 	{
-		k = i + j;
+		k = i + j0;
 		i = j;
 		j = k;
 		printf("%lu", k);
